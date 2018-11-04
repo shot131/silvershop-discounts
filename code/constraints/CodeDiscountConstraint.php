939af9453c41d372b941ef8009b0214b02402122
shot <shot131@gmail.com>
2018-11-04 20:58:35 +0300

    Add abiliti to CodeDiscountConstraint find multiple codes

--- code/constraints/CodeDiscountConstraint.php
+++ code/constraints/CodeDiscountConstraint.php
@@ -9,9 +9,16 @@ class CodeDiscountConstraint extends DiscountConstraint{
 	//cms field is added in OrderCoupon class
 
 	public function filter(DataList $list) {
-		if($code = $this->findCouponCode()){
-			$list = $list
-				->where("(\"Code\" IS NULL) OR (\"Code\" = '$code')");
+        $code = $this->findCouponCode();
+		if(!empty($code)){
+		    if (is_array($code)) {
+                $codeArr = Convert::raw2sql($code, true);
+                $list = $list
+                    ->where("(\"Code\" IS NULL) OR (\"Code\" IN (" . implode(',', $codeArr) . "))");
+            } else {
+                $list = $list
+                    ->where("(\"Code\" IS NULL) OR (\"Code\" = ".  Convert::raw2sql($code, true) . ")");
+            }
 		}else{
 			$list = $list->where("\"Code\" IS NULL");
 		}
@@ -20,8 +27,13 @@ class CodeDiscountConstraint extends DiscountConstraint{
 	}
 
 	public function check(Discount $discount) {
-		$code = strtolower($this->findCouponCode());
-		if($discount->Code && ($code != strtolower($discount->Code))){
+		$code = $this->findCouponCode();
+		if (is_array($code)) {
+		    foreach ($code as $k => $codeVal) {
+                $code[$k] = strtolower($codeVal);
+            }
+        }
+		if($discount->Code && (!in_array(strtolower($discount->Code), $code))){
 			$this->error(_t('CodeDiscountConstraint.ErrorCodeDontMatch',
                 "Coupon code doesn't match {code}",
                 array('code' => $code)
